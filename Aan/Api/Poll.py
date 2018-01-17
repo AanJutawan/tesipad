38a3aa0710b89ef8b5e19d93b6adb530d8cbf7ec
AanJutawan <33293274+AanJutawan@users.noreply.github.com>
2018-01-17 07:45:26 +0000

    Update Poll.py

--- Aan/Api/Poll.py
+++ Aan/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = " Line/7.18.0"
-  LA = "CHROMEOS 2.0.0 Chrome_OS 1"
+  UA = " Line/6.0.0 iPad4,1 9.0.2"
+  LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
 
   rev = 0
 
